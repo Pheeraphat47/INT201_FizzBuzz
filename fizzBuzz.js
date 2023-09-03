f0cef096ad664b2d683debbe5436226f7db021c2
Pheeraphat <dpheeraphat2547@gmail.com>
2023-09-03 13:47:49 +0700

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,4 +1,22 @@
 function fizzBuzz(max) {
   //code here
+
+  let result = "";
+
+  for (let i = 1; i <= max; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result += "FizzBuzz";
+    } else if (i % 3 === 0) {
+      result += "Fizz";
+    }
+    else if (i % 5 === 0) {
+      result += "Buzz";
+    } else {
+      result += i;
+    }
+
+  }
+
+  return result;
 }
 module.exports = fizzBuzz
